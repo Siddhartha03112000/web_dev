4421ade8e578a3492aadf39f6dd5f65c752e59e8
Siddhartha03112000 <65247431+Siddhartha03112000@users.noreply.github.com>
2021-01-26 22:52:14 +0530

    updated form_table

--- form_table.php
+++ form_table.php
@@ -17,51 +17,57 @@
             <option>B.Sc</option>
         </select> <br>
         Interest:
-        Reading<input type="checkbox" name="interest[]" value="Coding">
-        Gaming<input type="checkbox" name="interest[]" value="Gaming">
-        Swimming<input type="checkbox" name="interest[]" value="Reading">
-        Cooking<input type="checkbox" name="interest[]" value="Cooking">
-        Coding<input type="checkbox" name="interest[]" value="Teaching"> <br>
+        <input type="checkbox" name="interest[1]" value="Reading">
+        Gaming<input type="checkbox" name="interest[2]" value="Gaming">
+        Swimming<input type="checkbox" name="interest[3]" value="Swimming">
+        Cooking<input type="checkbox" name="interest[4]" value="Cooking">
+        Coding<input type="checkbox" name="interest[5]" value="Coding"> <br>
         <input type="submit" name="submit" value="SUBMIT">
     </form>
+ <?php
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $contact = $_POST['contact'];
+        $city = $_POST['city'];
+        $course = $_POST['course'];
+        $interest = $_POST['interest']; 
+        if (empty($interest) || sizeof($interest) < 3 || sizeof($interest) > 5) {
+            echo "Select at least 3 interests and maximum 5 interests";
+        } else {
+            echo "<table>";
+            echo "<tr>";
+            echo "<th colspan='6'>OUTPUT</th>";
+            echo "</tr>";
+
+            echo "<tr>";
+            echo "<th>Name</th>";
+            echo "<th>Email</th>";
+            echo "<th>Contact</th>";
+            echo "<th>City</th>";
+            echo "<th>Course</th>";
+            echo "<th>Interest</th>";
+            echo "</tr>";
+
+            echo "<tr>";
+            echo "<th>$name</th>";
+            echo "<th>$email</th>";
+            echo "<th>$contact</th>";
+            echo "<th>$city</th>";
+            echo "<th>$course</th>";
+            echo "<th>";
+            echo "<ul>";
+            foreach ($interest as $int) {
+                echo "<li>$int</li>";
+            }
+            echo "</ul";
+            echo "</th>";
+            echo "</tr>";
+            
+            echo "</table>";
+            
+        }
+    }
+?>
 </body>
 </html>
-
-<?php
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $contact = $_POST['contact'];
-    $city = $_POST['city'];
-    $course = $_POST['course'];
-    $interest = $_POST['interest'];
-    if (empty($_POST['name'])) {
-        echo "Empty 'Name' is not allowed.";
-    } else if (empty($_POST['email'])) {
-        echo "Empty 'Email' is not allowed.";
-    } else if (empty($_POST['contact'])) {
-        echo "Empty 'Contact' is not allowed.";
-    } else if (empty($_POST['course'])) {
-        echo "Empty 'Course' is not allowed.";
-    } else if (sizeof($interest) < 3) {
-        echo "At least select any 3 Interests.";
-    } else {
-        echo "Name : $name <br/>";
-        echo "email : $email <br/>";
-        echo "contact : $contact <br/>";
-        echo "course : $course <br/>";
-        echo "Interest : [";
-        $first = true;
-        foreach ($interest as $int) {
-            if ($first) {
-                $first = false;
-            } else {
-                echo ", ";
-            }
-            echo $int;
-        }
-        echo "]";
-    }
-}
-
-?>
